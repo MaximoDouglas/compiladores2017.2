7828282cf7c28b7f65b77fd2459a3aef2ee9d0de
MaximoDouglas <dhms@ic.ufal.br>
2018-03-08 12:48:40 -0300

    Recognition of the following categories: CTE_CAD_CH

--- compilador/src/main/java/br/ufal/ic/compilator/model/TokenService.java
+++ compilador/src/main/java/br/ufal/ic/compilator/model/TokenService.java
@@ -7,53 +7,53 @@ public class TokenService {
 	
 	public static void fillExpressoes () {
 		
-		expressoes.add("main".toString());
-		expressoes.add("[;]".toString());
-		expressoes.add("[,]".toString());
-		expressoes.add("[(]".toString());
-		expressoes.add("[)]".toString());
-		expressoes.add("[{]".toString());
-		expressoes.add("[}]".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("[=]".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("-".toString());
-		expressoes.add("int".toString());
-		expressoes.add("float".toString());
-		expressoes.add("boolean".toString());
-		expressoes.add("char".toString());
-		expressoes.add("void".toString());
-		expressoes.add("[+]".toString());
-		expressoes.add("[-]".toString());
-		expressoes.add("[*]".toString());
-		expressoes.add("[/]".toString());
-		expressoes.add("==".toString());
-		expressoes.add("!=".toString());
-		expressoes.add("[<]".toString());
-		expressoes.add("<=".toString());
-		expressoes.add("[>]".toString());
-		expressoes.add(">=".toString());
-		expressoes.add("-".toString());
-		expressoes.add("[&]".toString());
-		expressoes.add("[!]".toString());
-		expressoes.add("-".toString());
-		expressoes.add("return".toString());
-		expressoes.add("if".toString());
-		expressoes.add("else".toString());
-		expressoes.add("for".toString());
-		expressoes.add("while".toString());
-		expressoes.add("read".toString());
-		expressoes.add("print".toString());
-		expressoes.add("println".toString());
-		expressoes.add("-".toString());
+		expressoes.add("main"); //MAIN
+		expressoes.add("[;]"); //PONT_VIRG
+		expressoes.add("[,]"); //VIRGULA
+		expressoes.add("[(]"); //AB_PARENTE
+		expressoes.add("[)]"); //FE_PARENTE
+		expressoes.add("[{]"); //AB_CHAVE
+		expressoes.add("[}]"); //FE_CHAVE
+		expressoes.add("-"); //AB_COLCHET
+		expressoes.add("-"); //FE_COLCHET
+		expressoes.add("-"); //ID
+		expressoes.add("[=]"); //ATRIBUICAO
+		expressoes.add("-"); //CTE_INT
+		expressoes.add("-"); //CTE_FLOAT
+		expressoes.add("-"); //CTE_BOOL
+		expressoes.add("-"); //CTE_CHAR
+		expressoes.add("-"); //CTE_CAD_IN
+		expressoes.add("-"); //CTE_CAD_FL
+		expressoes.add("-"); //CTE_CAD_BO
+		expressoes.add("\"[^\\r\\n]*\""); //CTE_CAD_CH
+		expressoes.add("int"); //TIPO_INT
+		expressoes.add("float"); //TIPO_FLOAT
+		expressoes.add("boolean"); //TIPO_BOOL
+		expressoes.add("char"); //TIPO_CHAR
+		expressoes.add("void"); //TIPO_VOID
+		expressoes.add("[+]"); //OPA_AD
+		expressoes.add("[-]"); //OPA_SUB
+		expressoes.add("[*]"); //OPA_MULT
+		expressoes.add("-"); //OPA_DIV
+		expressoes.add("=="); //OPR_IGUAL
+		expressoes.add("!="); //OPR_DIF
+		expressoes.add("[<]"); //OPR_MEN
+		expressoes.add("<="); //OPR_MEN_IG
+		expressoes.add("[>]"); //OPR_MAI
+		expressoes.add(">="); //OPR_MAI_IG
+		expressoes.add("\\|"); //OPL_OU
+		expressoes.add("[&]"); //OPL_E
+		expressoes.add("[!]"); //OPL_NAO
+		expressoes.add("-"); //OP_CONC
+		expressoes.add("return"); //RETORNO
+		expressoes.add("if"); //SE
+		expressoes.add("else"); //SENAO
+		expressoes.add("for"); //FOR
+		expressoes.add("while"); //WHILE
+		expressoes.add("read"); //ENTRADA
+		expressoes.add("print"); //SAIDA
+		expressoes.add("println"); //SAIDA_LN
+		expressoes.add("//.*$"); //COMENTARIO
 	}
 	
 	public static ArrayList<String> getExpressoes(){
